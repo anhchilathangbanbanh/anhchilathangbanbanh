5afc7c716549bfccb13919efa943bdd10677f502
anhchilathangbanbanh <manh@Macbooks-MacBook-Air.local>
2017-07-22 20:28:56 +0700

    add view engine

--- app.js
+++ app.js
@@ -8,33 +8,34 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('views', __dirname + '/public');
+app.set('views', __dirname + '/view');
+app.use('/public', express.static(process.cwd() + '/public'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.post('/upload', function(req, res) {
-    var form = new multiparty.Form();
-    form.parse(req, function(err, fields, files) {
-       var audio = files.audio[0];
-       fs.readFile(audio.path, 'utf8', function(err, data) {
-           if (err) {
-              console.log('Error1: ' + err);
-          }else {
-             var path = './public/audio/' + audio.originalFilename;
-             fs.writeFile(path, data, function(err) {
-                if (err) {
-                    console.log('Error2: ' + err);
-                }else {
-                    res.send('Success');
-                }
-             });
-          }
-       })
-    });
-});
+// app.post('/upload', function(req, res) {
+//     var form = new multiparty.Form();
+//     form.parse(req, function(err, fields, files) {
+//        var audio = files.audio[0];
+//        fs.readFile(audio.path, 'utf8', function(err, data) {
+//            if (err) {
+//               console.log('Error1: ' + err);
+//           }else {
+//              var path = './public/audio/' + audio.originalFilename;
+//              fs.writeFile(path, data, function(err) {
+//                 if (err) {
+//                     console.log('Error2: ' + err);
+//                 }else {
+//                     res.send('Success');
+//                 }
+//              });
+//           }
+//        })
+//     });
+// });
 
 app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), () => {
