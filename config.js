f8ff2b616a540a273f8cdedb868b63fa2c31df69
anhchilathangbanbanh <manh@Macbooks-MacBook-Air.local>
2017-08-02 00:32:24 +0700

    fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     "PORT": 3000,
-    "DB_URL": process.env.MONGODB_URI || "mongodb://localhost:27017/anhchilathangbanbanh",
+    "DB_URL": process.env.MONGODB_URI,
     "numberOfDataDisplay": 20
 }
