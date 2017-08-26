19543b156375e7eaafe61eeff365c926b59073e3
DK2405 <sontvse04903@fpt.edu.vn>
2017-08-26 10:36:18 +0700

    fix header

--- banbanh_client/src/components/Header.js
+++ banbanh_client/src/components/Header.js
@@ -4,38 +4,6 @@ import $ from 'jquery';
 class Header extends Component {
     render() {
         return (
-<<<<<<< HEAD
-            <div className="Header">
-              <div class="row">
-                  <div class="col-sm-4 col-md-2 col-md-offset-2 logo">
-                      <a href="#"><img src="../public/images/new-logo.png" height="190"/></a>
-                  </div>
-                  <div class="col-sm-8 col-md-7 menu-wrapper">
-                      <nav class="navbar navbar-custom">
-                          <div class="navbar-header">
-                              <button class="collapsed navbar-toggle button-menu" data-toggle="collapse" data-target="#menu-collapse">
-                                  <span class="sr-only">Toggle</span>
-                                  <span class="icon-bar"></span>
-                                  <span class="icon-bar"></span>
-                                  <span class="icon-bar"></span>
-                              </button>
-                              <span class="basket">
-                                  <a href="#"><img src="../public/images/basket.png"/></a>
-                              </span>
-                          </div>
-
-                          <div class="navbar-collapse collapse" id="menu-collapse">
-                              <ul class="nav navbar-nav menu-detail">
-                                  <li><a href="#">Home</a></li>
-                                  <li><a href="#">Contact us</a></li>
-                                  <li><a href="#">New Order</a></li>
-                                  <li><a href="#">Shopping Cart <span class="glyphicon glyphicon-shopping-cart"></span></a></li>
-                              </ul>
-                          </div>
-                      </nav>
-                  </div>
-              </div>
-=======
           <div className="Header">
                 <div className="container">
                     <div className="row">
@@ -52,7 +20,6 @@ class Header extends Component {
                                         <span className="icon-bar"></span>
                                     </button>
                                 </div>
-
                                 <div className="navbar-collapse collapse" id="menu-collapse">
                                     <ul className="nav navbar-nav MenuDetail">
                                         <li><a href="#">Home</a></li>
@@ -65,7 +32,6 @@ class Header extends Component {
                         </div>
                     </div>
                 </div>
->>>>>>> 76e760df232f95ea3561cb0069989b9df07c24fa
             </div>
         );
     }
